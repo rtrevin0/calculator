d1b5095130f73820184d3f3a89a1d53021e2a2c8
rtrevin0 <rickeytrevino@gmail.com>
2026-01-23 23:39:50 -0600

    add basic calculator functions

---
+++ app.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return x / y;
+}
+
+// Export functions for use in other modules
+export { add, subtract, multiply, divide };
